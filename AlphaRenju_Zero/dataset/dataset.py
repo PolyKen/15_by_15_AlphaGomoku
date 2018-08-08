dfd65694130f3f18b2010021a7796dc2295b858d
Zheng Xie <kennyxz@live.cn>
2018-08-08 22:35:38 +0800

    update

--- AlphaRenju_Zero/dataset/dataset.py
+++ AlphaRenju_Zero/dataset/dataset.py
@@ -13,19 +13,49 @@ class DataSet:
         self._game_record.append(record)
         
     def get_sample(self, percentage):
-        obs = []
-        col = []
-        last_move = []
-        pi = []
-        z = []
+        obs_b = []
+        col_b = []
+        last_move_b = []
+        pi_b = []
+        z_b = []
+        obs_w = []
+        col_w = []
+        last_move_w = []
+        pi_w = []
+        z_w = []
         for record in self._game_record:
             a, b, c, d, e = record.get_sample(percentage)
-            obs.extend(a)
-            col.extend(b)
-            last_move.extend(c)
-            pi.extend(d)
-            z.extend(e)
-        return obs, col, last_move, pi, z
+            if e == 1:
+                obs_b.extend(a)
+                col_b.extend(b)
+                last_move_b.extend(c)
+                pi_b.extend(d)
+                z_b.extend(e)
+            else:
+                obs_w.extend(a)
+                col_w.extend(b)
+                last_move_w.extend(c)
+                pi_w.extend(d)
+                z_w.extend(e)
+
+        b_size = len(z_b)
+        w_size = len(z_w)
+        ratio = b_size // w_size
+
+        # balancing data
+        obs_w = obs_w * ratio
+        col_w = col_w * ratio
+        last_move_w = last_move_w * ratio
+        pi_w = pi_w * ratio
+        z_w = z_w * ratio
+
+        obs_b.extend(obs_w)
+        col_b.extend(col_w)
+        last_move_b.extend(last_move_w)
+        pi_b.extend(pi_w)
+        z_b.extend(z_w)
+
+        return obs_b, col_b, last_move_b, pi_b, z_b
 
     def record_num(self):
         return len(self._game_record)
